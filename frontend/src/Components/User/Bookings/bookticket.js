0b3d463767f0fee4b080f5ca8549db4e4d4b65e0
Allwin E K <ekallwin@gmail.com>
2025-04-25 20:39:36 +0530

    Changes

--- frontend/src/Components/User/Bookings/bookticket.js
+++ frontend/src/Components/User/Bookings/bookticket.js
@@ -113,7 +113,6 @@ const TrainBooking = () => {
       from,
       to,
       quota,
-      selectedDate,
     }));
     navigate("/train-search", {
       state: { from, to, date: formatDate(selectedDate), quota }
